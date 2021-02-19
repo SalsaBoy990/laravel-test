1aef0f7140b56c553dc39e38eeb8848d7fba93dc
Gulácsi András <guland@protonmail.com>
2021-02-19 01:02:46 +0100

    new: Címkék CRUD megvalósítása

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = Tag::all();
+
+        return view('tag.index')->with([
+            'tags' => $tags
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tag.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validáció
+        $request->validate([
+            'name' => 'required|string|min:3|max:60|unique:tags,name',
+            'description' => 'sometimes|min:10|string|nullable',
+        ]);
+
+        // létrehozás
+        $tag = new Tag([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'slug' => Str::slug($request->input('name'), '-'),
+        ]);
+
+        // mentés
+        $tag->save();
+
+        // átirányítás
+        return $this->index()->with([
+            'message_success' => "The tag <b>" . $tag->name . "</b> was created."
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tag  $tags
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tag $tag)
+    {
+        return view('tag.show')->with([
+            'tag' => $tag
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tag  $tags
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tag $tag)
+    {
+        return view('tag.edit')->with([
+            'tag' => $tag
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tag $tag)
+    {
+        // validáció
+        $request->validate([
+            'name' => 'required|string|min:3|max:60|unique:tags,name',
+            'description' => 'sometimes|min:10|string|nullable',
+        ]);
+
+        // frissítés
+        $tag->update([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'slug' => Str::slug($request->input('name'), '-'),
+        ]);
+
+
+        // átirányítás
+        return $this->index()->with([
+            'message_success' => "The tag <b>" . $tag->name . "</b> was updated."
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tag $tag)
+    {
+        $oldName = $tag->name;
+        $tag->delete();
+
+        return $this->index()->with([
+            'message_success' => "The tag <b>" . $oldName . "</b> was deleted."
+        ]);
+    }
+}

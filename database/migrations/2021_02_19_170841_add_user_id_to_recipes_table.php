1c4844c0a82c4a187f37de0fe28ae78b2de222f2
Gulácsi András <guland@protonmail.com>
2021-02-24 21:14:31 +0100

    Migrációk módosítása
    update: Users táblában motto, about_me oszlopok hozzáadása
    update: A recipes táblához user_id foreign key oszlop hozzáadása
    new: Kapcsolótábla létrehozása recipe_tag néven

--- database/migrations/2021_02_19_170841_add_user_id_to_recipes_table.php
+++ database/migrations/2021_02_19_170841_add_user_id_to_recipes_table.php
@@ -14,7 +14,13 @@ class AddUserIdToRecipesTable extends Migration
     public function up()
     {
         Schema::table('recipes', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('user_id')
+            ->after('id')
+            ->nullable();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
         });
     }
 
@@ -26,7 +32,8 @@ class AddUserIdToRecipesTable extends Migration
     public function down()
     {
         Schema::table('recipes', function (Blueprint $table) {
-            //
+            $table->dropForeign( ['user_id'] );
+            $table->dropColumn('user_id');
         });
     }
 }

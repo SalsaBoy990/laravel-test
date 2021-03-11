c343b500eb96aa4799565d6ec3b6ff47d3d4a460
Gulácsi András <guland@protonmail.com>
2021-03-12 00:36:39 +0100

    new: Képkezelés megvalósítása a recepteknél is

--- resources/views/recipe/index.blade.php
+++ resources/views/recipe/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container py-5">
   <div class="row justify-content-center">
-    <div class="col-md-11">
-      <div class="card">
+    <div class="col-md-12">
+      <div class="card shadow border-0">
       @isset($filter)
       <div class="card-header">{{ __('Filtered recipes by') }} <span class="badge badge-{{ $filter->style }}">{{ $filter->name }}</span>
         <a href="/recipe" class="float-right medium">{{ __('Show all recipes') }}</a>
       </div>
       @else
-      <div class="card-header">{{ __('Recipes') }}</div>
+      <div class="card-header cart-header-page-title uppercase">{{ __('Recipes') }}</div>
       @endisset
         <div class="card-body">
           @auth
@@ -20,23 +20,38 @@
           @endauth
           <ul class="list-group list-group-no-bullets">
             @foreach($recipes as $recipe)
-            <li class="list-group-element mt-1" style="padding: 5px;">
-              <a title="Show details" class="recipe-title-link" style="font-weight: bold;" href="/recipe/{{ $recipe->id }}">{{ $recipe->name }}</a>
+            <li class="list-group-item mt-1 border-top-0 border-right-0 border-left-0" style="padding: 5px;">
+              @if (file_exists('img/recipes/' . $recipe->id . '_thumb.jpg'))
+              <a title="Show details" class="recipe-title-link" style="font-weight: bold;text-decoration: none !important;" href="/recipe/{{ $recipe->id }}">
+                <img class="rounded mr-1" src="/img/recipes/{{ $recipe->id }}_thumb.jpg" alt="recipe thumbnail">
+              </a>
+              @endif
+              <h2 class="h3" style="display: inline-block">
+              <a title="Show details" class="recipe-title-link" style="font-weight: bold;" href="/recipe/{{ $recipe->id }}">
+                {{ $recipe->name }}</a>
+              </h2>
               <br>
-              <span style="font-size: 12px; color: #666;">by: <a href="/user/{{ $recipe->user_id }}">{{ $recipe->user->name }}</a><span style="font-size: 10px; color: #666;"> ({{ $recipe->user->recipes->count() }} Recipes)</span></span></span>
+              <span class="small">
+                  by: <a href="/user/{{ $recipe->user_id }}">{{ $recipe->user->name }}</a><span style="font-size: 10px;"> ({{ $recipe->user->recipes->count() }} Recipes)</span></span>
+            </span>
+            @if (file_exists('img/users/' . $recipe->user_id . '_thumb.jpg'))
+            <a title="Show details" class="recipe-title-link" style="font-weight: bold;text-decoration: none !important;" href="/user/{{ $recipe->user_id }}">
+              <img class="rounded mr-1" src="/img/users/{{ $recipe->user_id }}_thumb.jpg" alt="author thumbnail">
+            </a>
+            @endif
 
               @auth
               <form style="display: inline;" class="float-right" action="/recipe/{{ $recipe->id }}" method="post">
                 @csrf()
                 @method('DELETE')
-                <button onClick="confirm('Are you sure you want to delete {{ $recipe->name }}');" type="submit" class="btn btn-sm btn-outline-danger uppercase">
+                <button onClick="confirm('Are you sure you want to delete {{ $recipe->name }}');" type="submit" class="btn btn-sm btn-warning uppercase">
                  <i class="fas fa-trash"></i> Delete
                 </button>
-                <a href="/recipe/{{ $recipe->id }}/edit" class="btn btn-sm btn-light mr-2 uppercase"><i class="fas fa-edit"></i> Edit</a>
+                <a href="/recipe/{{ $recipe->id }}/edit" class="btn btn-sm btn-info mr-2 uppercase"><i class="fas fa-edit"></i> Edit</a>
               </form>
               @endauth
               <br>
-              <span style="font-size: 12px; color: #999;">{{ $recipe->created_at->diffForHumans() }}</span>
+              <span style="font-size: 12px">{{ $recipe->created_at->diffForHumans() }}</span>
               <br>
               @foreach ($recipe->tags as $tag)
                 <a href="/recipe/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a> 
@@ -47,7 +62,7 @@
         </div>
       </div>
 
-      <div class="mt-3">
+      <div class="mt-5">
         {{ $recipes->links() }}
       </div>
 
